cff7b2c242085fb33140def0c3f17d0cd9e5481c
Ben Hamilton <ben.hamilton@me.com>
2019-05-13 02:43:03 +0100

    added delete button and some css

--- js/app.js
+++ js/app.js
@@ -4,29 +4,37 @@ document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('#new-item-form');
   form.addEventListener('submit', handleFromSubmit)
   
+  const deleteAllButton = document.querySelector('#delete-all');
+  deleteAllButton.addEventListener('click', handleDeleteAllClick)
   })
 
   const handleFromSubmit = function(event){
     event.preventDefault()
-  
+      
     const locate = document.querySelector('#hero-list')
+    
     const newListItem = document.createElement('li')
+    locate.appendChild(newListItem)
+
     const name = document.createElement('h1')
     name.textContent = `${event.target.name.value}`
-  
+    newListItem.appendChild(name)
+
+    
     const nationality = document.createElement('h2')
     nationality.textContent = `${event.target.nationality.value}`
+    newListItem.appendChild(nationality)
   
     const role = document.createElement('p')
     role.textContent = `${event.target.role.value}`
-  
-    
-  
-    locate.appendChild(newListItem)
-    newListItem.appendChild(name)
-    newListItem.appendChild(nationality)
     newListItem.appendChild(role)
-    const form = document.querySelector('#new-item-form')
-    form.reset()
+    
+    event.target.reset()
+
+  }
+
+  const handleDeleteAllClick = function (event) {
+    const readingList = document.querySelector('#hero-list');
+    readingList.innerHTML = '';
   }
   
